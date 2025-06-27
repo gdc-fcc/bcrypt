d1b1f3c0586e1fd7fc4842ddbacbcc4da74fe464
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-28 00:08:36 +0200

    finalize challenge (hash/compare sync/async)

--- server.js
+++ server.js
@@ -13,13 +13,26 @@ const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+      });
+    bcrypt.compare('wrong pass', hash, (err, res) => {
+        console.log(res);
+      });
+});
 
 
 //END_ASYNC
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
